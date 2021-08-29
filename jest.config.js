d3aa5587ba0ef3c8ff57807fd95449decd8592a3
Jeff Registre <jeffreyregistre@gmail.com>
2021-08-29 18:21:55 -0400

    dynamic programming folder

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-    transform: {
-        "^.+\\.tsx?$": "ts-jest",
-    },
-    testRegex: "/ts/.*",
-    testPathIgnorePatterns: ["/lib/", "/node_modules/"],
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-    // collectCoverage: true,
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  // testRegex: "/ts/**/*.ts",
+  testMatch: ["**/ts/**/*.ts"],
+  testPathIgnorePatterns: ["/lib/", "/node_modules/"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  // collectCoverage: true,
 };
