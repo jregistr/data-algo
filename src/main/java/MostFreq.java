ae855b0f34ab6f125d92a1edff25ecf555b12097
Jeff Registre <jeffreyregistre@gmail.com>
2021-09-02 21:25:37 -0400

    Test

---
+++ src/main/java/MostFreq.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MostFreq {
+
+    public int mostFreq(int[] numbers) {
+        return 1;
+        // if(numbers.length == 0)  return 0;
+        // Map<Integer, Integer> found = new HashMap<>();
+
+        // int mostOcc = numbers[0];
+        // int mostOccCount = 1;
+
+        // for(int i = 1; i < numbers.length; i ++) {
+        //     int num = numbers[i];
+
+        // }
+    }
+}
