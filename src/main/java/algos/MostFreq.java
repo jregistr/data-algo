ca50affa757ee133325a2aba891bad9dac49bb7b
Jeff Registre <jeffreyregistre@gmail.com>
2021-09-03 01:08:17 -0400

    Most frequently occuring java

--- src/main/java/algos/MostFreq.java
+++ src/main/java/algos/MostFreq.java
@@ -6,16 +6,21 @@ import java.util.Map;
 public class MostFreq {
 
     public int mostFreq(int[] numbers) {
-        return 1;
-        // if(numbers.length == 0)  return 0;
-        // Map<Integer, Integer> found = new HashMap<>();
+        if(numbers.length == 0)  return 0;
+        Map<Integer, Integer> found = new HashMap<>();
 
-        // int mostOcc = numbers[0];
-        // int mostOccCount = 1;
+        int mostOcc = numbers[0];
+        int mostOccCount = 1;
+        found.put(mostOcc, mostOccCount);
 
-        // for(int i = 1; i < numbers.length; i ++) {
-        //     int num = numbers[i];
-
-        // }
+        for(int i = 1; i < numbers.length; i ++) {
+            int num = numbers[i];
+            int foundCount = found.compute(num, (k, v) -> v == null ? 1 : v + 1);
+            if(foundCount > mostOccCount) {
+                mostOccCount = foundCount;
+                mostOcc = num;
+            }
+        }
+        return mostOcc;
     }
 }
